e313e4698304c0bad87068d1faad74900c33d139
Aida Šertović Komić <sertovicaida@gmail.com>
2023-04-30 21:01:09 +0200

    fetch recipe data from api and display them in popular component

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
+import Wrapper from "./UI/Wrapper/Wrapper";
+
 const Popular = () => {
+
+    const [popular, setPopular] = useState([]);
+
+    useEffect(() => {
+        getPopular();
+    }, []);
+
+    const getPopular = async () => {
+        try {
+            const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_KEY}&number=9`)
+            const data = await api.json();
+            setPopular(data.recipes);
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
-    <div>Popular</div>
-)
+        <div>
+            {popular?.map((recipe) => {
+                return (
+                    <Wrapper key={recipe?.id}>
+                        <p>{recipe?.title}</p>
+                    </Wrapper>
+
+                )
+            })
+            }
+        </div>
+    )
 }
 
 export default Popular;
+
---
+++ src/components/UI/Wrapper/Wrapper.jsx
@@ -0,0 +1,9 @@
+import './Wrapper.module.css';
+
+const Wrapper = (props) => {
+    return (
+        <div>{props.children}</div>
+    )
+}
+
+export default Wrapper;
