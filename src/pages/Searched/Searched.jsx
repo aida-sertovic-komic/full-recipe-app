cb818eda43cc12bc65d39fc5b9552d9cc4653e4c
Aida Šertović Komić <sertovicaida@gmail.com>
2023-05-06 20:02:59 +0200

    display searched recipes

--- src/pages/Searched/Searched.jsx
+++ src/pages/Searched/Searched.jsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Card from './../../components/UI/Card/Card';
+import Grid from './../../components/UI/Grid/Grid';
 import './Searched.module.css';
 
 const Searched = () => {
+    const [searchedRecipes, setSearchedRecipes] = useState([]);
+    let params = useParams();
+
+    const getSearched = async (name) => {
+        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_KEY}&query=${name}`);
+        const recipes = await data.json();
+        setSearchedRecipes(recipes.results)
+    }
+
+    useEffect(() => {
+        getSearched(params.searchTerm);
+        console.log(searchedRecipes)
+    }, [params.searchTerm])
+
     return (
-        <div></div>
+        <Grid>
+        {searchedRecipes.map((item) => {
+            return (
+                <Card key={item.id}>
+                    <img src={item.image} alt={item.title} />
+                    <h4>{item.title}</h4>
+                </Card>
+            )
+        })}
+    </Grid>
     )
 }
 
